e0eb17018a5b5e85fe6087358489e3c84782af08
Michael Ma <ma.2374@osu.edu>
2025-04-02 03:03:33 -0400

    initial commit

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+import { generateColorPalette } from "./src/utils/themeColor";
+
+const theme_color = "#FFAAA7";
+const theme_color_hue_shift = 30;
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: ["class"],
+  theme: {
+    extend: {
+      "colors": {
+        "theme": generateColorPalette(theme_color, theme_color_hue_shift)
+      }
+    }
+  },
+  plugins: [
+      require("@tailwindcss/typography"),
+  ],
+};
+export default config;
