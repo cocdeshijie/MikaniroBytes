c606a3dc6007ec81f7c682a0d2b0ca7490002d85
Michael Ma <ma.2374@osu.edu>
2025-04-25 04:35:40 -0400

    🎨 style: update theme color and hue shift for improved design consistency

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "tailwindcss";
 import { generateColorPalette } from "./src/utils/themeColor";
 
-const theme_color = "#FFAAA7";
-const theme_color_hue_shift = 30;
+const theme_color = "#F08300";
+const theme_color_hue_shift = 20;
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
